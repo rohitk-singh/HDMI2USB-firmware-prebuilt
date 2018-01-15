b03fdedaec6d807db7d41be9500bf65005113e0a
Tim 'mithro' Ansell <me@mith.ro>
2018-01-15 06:25:04 +0000

    Travis build #5 of rohit-fix-arty/v0.0.4-64-ga1ee3d5 for PLATFORM=minispartan6 TARGET=base CPU=lm32 FIRMWARE=firmware
    From https://github.com/rohitk-singh/litex-buildenv/tree/a1ee3d52f4f1836ca2690af088738a228e9077e2

---
+++ archive/rohit-fix-arty/v0.0.4-64-ga1ee3d5/minispartan6/base/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_MINISPARTAN6
+#error "Version mismatch - PLATFORM_MINISPARTAN6 not defined!"
+#endif
+const char* board = "minispartan6";
+
+#ifndef TARGET_BASE
+#error "Version mismatch - TARGET_BASE not defined!"
+#endif
+const char* target = "base";
+
+const char* git_commit = "a1ee3d52f4f1836ca2690af088738a228e9077e2";
+const char* git_branch = "rohit-fix-arty";
+const char* git_describe = "v0.0.4-64-ga1ee3d5";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
