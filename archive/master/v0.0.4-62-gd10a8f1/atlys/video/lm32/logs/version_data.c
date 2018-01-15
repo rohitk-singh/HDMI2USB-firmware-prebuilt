e2886a43e83f495a3ccbb05fe0eee8d3329d6d08
Rohit Kumar Singh <rohit91.2008@gmail.com>
2018-01-15 06:51:20 +0000

    Travis build #3 of master/v0.0.4-62-gd10a8f1 for PLATFORM=atlys TARGET=video CPU=lm32 FIRMWARE=firmware
    From https://github.com/rohitk-singh/litex-buildenv/tree/d10a8f1f222ef4354e63dec633a7334005828fd6

---
+++ archive/master/v0.0.4-62-gd10a8f1/atlys/video/lm32/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_ATLYS
+#error "Version mismatch - PLATFORM_ATLYS not defined!"
+#endif
+const char* board = "atlys";
+
+#ifndef TARGET_VIDEO
+#error "Version mismatch - TARGET_VIDEO not defined!"
+#endif
+const char* target = "video";
+
+const char* git_commit = "d10a8f1f222ef4354e63dec633a7334005828fd6";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-62-gd10a8f1";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
